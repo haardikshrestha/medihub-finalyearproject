61f3b24455b4110e63058a20483ec8aea3ff0245
haardikshrestha <haardikshrestha@gmail.com>
2024-01-04 11:06:34 +0545

    ui for tables

--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
-// server.js
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
-const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const User = require("./models/userSchema");
 const app = express();
-const nodemailer = require("nodemailer");
-// require('dotenv').config();
 
 const dbURI =
   "mongodb+srv://medihub:medihub%40123@cluster0.a1uktfi.mongodb.net/UsersDB?retryWrites=true&w=majority";
@@ -30,18 +26,6 @@ mongoose
 app.use(bodyParser.json());
 app.use(cors());
 
-// const transporter = nodemailer.createTransport({
-//   host: process.env.HOST,
-//   service: process.env.SERVICE,
-//   port: process.env.EMAIL_PORT,
-//   secure: process.env.SECURE === "true", // convert to boolean
-//   auth: {
-//     user: process.env.USER,
-//     pass: process.env.PASS,
-//   },
-// });
-
-
 async function hashPassword(password) {
   const hashedPassword = await bcrypt.hash(password, saltRounds);
   return hashedPassword;
@@ -51,74 +35,6 @@ async function comparePassword(inputPassword, storedHash) {
   const isPasswordValid = await bcrypt.compare(inputPassword, storedHash);
   return isPasswordValid;
 }
-app.get('/reset-password', (req, res) => {
-  // Serve the reset password page or redirect to a frontend route
-  // This is where users will input their new password
-  // You might render an HTML page or redirect to your frontend route
-  res.status(200).send('Reset password page or redirect');
-});
-
-// app.post('/reset-password', async (req, res) => {
-//     try {
-//         const { email } = req.body;
-//         const user = await User.findOne({ email });
-
-//         if (!user) {
-//             return res.status(404).json({ error: 'User not found!' });
-//         }
-
-//         const resetToken = crypto.randomBytes(32).toString('hex');
-
-//         user.resetToken = resetToken;
-//         user.resetTokenExpires = Date.now() + 3600000; // Token expires in 1 hour
-//         await user.save();
-
-//         const resetLink = `http://your-app-url/reset-password?token=${resetToken}`;
-
-//         const mailOptions = {
-//           from: process.env.USER,
-//           to: user.email,
-//           subject: 'Password Reset Request',
-//           html: `Click the following link to reset your password: <a href="${resetLink}">${resetLink}</a>`
-//         };
-
-//         transporter.sendMail(mailOptions, (error, info) => {
-//           if (error) {
-//             console.error('Error sending email:', error);
-//             return res.status(500).json({ error: 'Error sending reset email!' });
-//           }
-//           console.log('Email sent:', info.response);
-//           res.status(200).json({ message: 'Reset link sent successfully!' });
-//         });
-//     } catch (error) {
-//         console.error('Reset Password Error:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-// app.post('/update-password', async (req, res) => {
-//     try {
-//         const { token, newPassword } = req.body;
-
-//         // Find the user by the reset token
-//         const user = await User.findOne({ resetToken: token, resetTokenExpires: { $gt: Date.now() } });
-
-//         if (!user) {
-//             return res.status(404).json({ error: 'Invalid or expired reset token!' });
-//         }
-
-//         // Update the user's password
-//         const hashedPassword = await hashPassword(newPassword);
-//         user.password = hashedPassword;
-//         user.resetToken = undefined;
-//         user.resetTokenExpires = undefined;
-//         await user.save();
-
-//         res.status(200).json({ message: 'Password updated successfully!' });
-//     } catch (error) {
-//         console.error('Update Password Error:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
 
 app.post("/register", async (req, res) => {
   try {
